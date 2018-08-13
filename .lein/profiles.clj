1c0d0ff85a4c3256eac24ecc4f6d30922e757c83
Aleksey Pirogov <astynax@users.noreply.github.com>
2018-08-14 00:33:06 +0300

    lein: actualize plugins

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,14 +1,10 @@
-{:user {:plugins [[lein-pprint "1.1.2"]
-                  [lein-kibit "0.1.2"]
+{:user {:plugins [[lein-pprint "1.2.0"]
+                  [lein-kibit "0.1.6"]
                   [lein-try "0.4.3"]
-                  [lein-exec "0.3.5"]
-                  [jonase/eastwood "0.2.1"]
-                  [cider/cider-nrepl "0.13.0-SNAPSHOT"]
-                  ;;[cider/cider-nrepl "0.9.1"]
-                  ;;[refactor-nrepl "2.0.0-SNAPSHOT"]
-                  [lein-ancient "0.6.10"]
+                  [jonase/eastwood "0.2.7"]
+                  [lein-ancient "0.6.15"]
                   ]
         :dependencies [[slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}
- :repl {:dependencies [^:displace [org.clojure/clojure "1.8.0"]]}
+ :repl {:dependencies [^:displace [org.clojure/clojure "1.9.0"]]}
  }
