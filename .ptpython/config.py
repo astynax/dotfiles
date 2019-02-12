7077c65846239ffb240017c5b7f7e9a47c9849e8
Aleksey Pirogov <astynax@users.noreply.github.com>
2019-02-12 09:38:46 +0300

    ptpython: actualize the config

--- .ptpython/config.py
+++ .ptpython/config.py
@@ -1,53 +1,125 @@
-# -*- coding: utf-8 -*-
+"""
+Configuration example for ``ptpython``.
+"""
+from __future__ import unicode_literals
+from prompt_toolkit.filters import ViInsertMode
+from prompt_toolkit.key_binding.key_processor import KeyPress
+from prompt_toolkit.keys import Keys
 from pygments.token import Token
-from ptpython.style import default_ui_style
+
+from ptpython.layout import CompletionVisualisation
 
 __all__ = (
     'configure',
 )
 
-# Solarized theme colors
-BASE03 = "#002B36"
-BASE02 = "#073642"
-BASE01 = "#586e75"
-BASE00 = "#657b83"
-BASE0 = "#839496"
-BASE1 = "#93a1a1"
-BASE2 = "#eee8d5"
-BASE3 = "#fdf6e3"
-YELLOW = "#b58900"
-ORANGE = "#cb4b16"
-RED = "#dc322f"
-MAGENTA = "#d33682"
-VIOLET = "#6c71c4"
-BLUE = "#268bd2"
-CYAN = "#2aa198"
-GREEN = "#859900"
-
-fmt = "bg:{} {}".format
-
-TOOLBAR_COMMON = fmt(BASE01, BASE2)
-TOOLBAR_SELECT = fmt(BASE2, BASE01)
-
 
 def configure(repl):
+    """
+    Configuration method. This is called during the start-up of ptpython.
+
+    :param repl: `PythonRepl` instance.
+    """
+    # Show function signature (bool).
+    repl.show_signature = True
+
+    # Show docstring (bool).
+    repl.show_docstring = True
+
+    # Show the "[Meta+Enter] Execute" message when pressing [Enter] only
+    # inserts a newline instead of executing the code.
+    repl.show_meta_enter_message = True
+
+    # Show completions. (NONE, POP_UP, MULTI_COLUMN or TOOLBAR)
+    repl.completion_visualisation = CompletionVisualisation.MULTI_COLUMN
+
+    # Show status bar.
+    repl.show_status_bar = True
+
+    # When the sidebar is visible, also show the help text.
+    repl.show_sidebar_help = True
+
+    # Highlight matching parethesis.
+    repl.highlight_matching_parenthesis = True
+
+    # Line wrapping. (Instead of horizontal scrolling.)
+    repl.wrap_lines = False
+
+    # Mouse support.
+    repl.enable_mouse_support = True
+
+    # Complete while typing. (Don't require tab before the
+    # completion menu is shown.)
     repl.complete_while_typing = False
-    repl.install_ui_colorscheme('my-colorscheme', _custom_ui_colorscheme)
-    repl.use_ui_colorscheme('my-colorscheme')
 
+    # Don't insert a blank line after the output.
+    repl.insert_blank_line_after_output = False
 
-_custom_ui_colorscheme = {}
-_custom_ui_colorscheme.update(default_ui_style)
-_custom_ui_colorscheme.update({
-    Token.Menu.Completions.Completion: TOOLBAR_COMMON,
-    Token.Menu.Completions.Completion.Current: TOOLBAR_SELECT,
-    Token.Menu.Completions.Meta: TOOLBAR_COMMON,
-    Token.Menu.Completions.Meta.Current: TOOLBAR_SELECT,
-    Token.Toolbar.Completions: TOOLBAR_COMMON,
-    Token.Toolbar.Completions.Arrow: TOOLBAR_SELECT,
-    Token.Toolbar.Completions.Completion: TOOLBAR_COMMON,
-    Token.Toolbar.Completions.Completion.Current: TOOLBAR_SELECT,
-    Token.Toolbar.Signature: TOOLBAR_COMMON,
-    Token.Toolbar.Signature.CurrentName: TOOLBAR_SELECT,
-    Token.Toolbar.Signature.Operator: fmt(BASE01, BASE3),
-})
+    # History Search.
+    # When True, going back in history will filter the history on the records
+    # starting with the current input. (Like readline.)
+    # Note: When enable, please disable the `complete_while_typing` option.
+    #       otherwise, when there is a completion available, the arrows will
+    #       browse through the available completions instead of the history.
+    repl.enable_history_search = True
+
+    # Enable auto suggestions. (Pressing right arrow will complete the input,
+    # based on the history.)
+    repl.enable_auto_suggest = False
+
+    # Enable open-in-editor. Pressing C-X C-E in emacs mode or 'v' in
+    # Vi navigation mode will open the input in the current editor.
+    repl.enable_open_in_editor = True
+
+    # Enable system prompt. Pressing meta-! will display the system prompt.
+    # Also enables Control-Z suspend.
+    repl.enable_system_bindings = True
+
+    # Ask for confirmation on exit.
+    repl.confirm_exit = True
+
+    # Enable input validation. (Don't try to execute when the input contains
+    # syntax errors.)
+    repl.enable_input_validation = True
+
+    # Use this colorscheme for the code.
+    repl.use_code_colorscheme('pastie')
+
+    # Set color depth (keep in mind that not all terminals support true color).
+
+    #repl.color_depth = 'DEPTH_1_BIT'  # Monochrome.
+    #repl.color_depth = 'DEPTH_4_BIT'  # ANSI colors only.
+    repl.color_depth = 'DEPTH_8_BIT'  # The default, 256 colors.
+    #repl.color_depth = 'DEPTH_24_BIT'  # True color.
+
+    # Syntax.
+    repl.enable_syntax_highlighting = True
+
+    # Add custom key binding for PDB.
+    """
+    @repl.add_key_binding(Keys.ControlB)
+    def _(event):
+        ' Pressing Control-B will insert "pdb.set_trace()" '
+        event.cli.current_buffer.insert_text('\nimport pdb; pdb.set_trace()\n')
+    """
+
+    # Custom key binding for some simple autocorrection while typing.
+    """
+    corrections = {
+        'impotr': 'import',
+        'pritn': 'print',
+    }
+
+    @repl.add_key_binding(' ')
+    def _(event):
+        ' When a space is pressed. Check & correct word before cursor. '
+        b = event.cli.current_buffer
+        w = b.document.get_word_before_cursor()
+
+        if w is not None:
+            if w in corrections:
+                b.delete_before_cursor(count=len(w))
+                b.insert_text(corrections[w])
+
+        b.insert_text(' ')
+    """
