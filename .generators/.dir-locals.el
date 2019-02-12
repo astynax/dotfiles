44daee3dc7988ac71dea085452b8571381242bbb
Aleksey Pirogov <astynax@users.noreply.github.com>
2019-02-12 09:34:54 +0300

    generators: tweak emacs setup

---
+++ .generators/.dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((haskell-mode
+  (haskell-hayoo-url . "https://www.stackage.org/lts-11.0/hoogle?q=%s")))
