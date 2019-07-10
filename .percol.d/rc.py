861a5e787e2a77febb91d53e4b8fcf490a533a1d
Aleksei Pirogov <astynax@users.noreply.github.com>
2019-07-10 09:50:46 +0300

    percol: remove config

--- .percol.d/rc.py
+++
@@ -1,4 +0,0 @@
-# percol.view.CANDIDATES_LINE_BASIC    = ("default", "on_default")
-percol.view.CANDIDATES_LINE_SELECTED = ("underline", "on_black")
-percol.view.CANDIDATES_LINE_MARKED   = ("bold", "yellow", "on_blue")
-percol.view.CANDIDATES_LINE_QUERY    = ("bold", "cyan")
