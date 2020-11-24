950afff8245c36552476f9ab767d57ffcd20ec71
Aleksei Pirogov <astynax@users.noreply.github.com>
2020-11-24 18:41:55 +0300

    [nix] add an initial version of nixpkgs configuration

---
+++ .config/nixpkgs/config.nix
@@ -0,0 +1,38 @@
+{
+  packageOverrides = pkgs: with pkgs; rec {
+    myProfile = writeText "my-profile" ''
+      export PATH=$HOME/.nix-profile/bin:/nix/var/nix/profiles/default/bin:/sbin:/bin:/usr/sbin:/usr/bin
+      export MANPATH=$HOME/.nix-profile/share/man:/nix/var/nix/profiles/default/share/man:/usr/share/man
+    '';
+
+    myPackages = pkgs.buildEnv {
+      name = "my-packages";
+      paths = [
+        (runCommand "profile" {} ''
+          mkdir -p $out/etc/profile.d
+          cp ${myProfile} $out/etc/profile.d/my-profile.sh
+        '')
+        # nix itself
+        nix
+        # locales for glibc
+        glibcLocales
+        # cli tools
+        ag
+        bat
+        fd
+        ripgrep
+        rq
+        starship
+        # dev tools
+        github-cli
+        shellcheck
+        upx
+        # apps
+        ncmpcpp
+        rofi
+      ];
+    };
+    pathsToLink = [ "/share/man" "/share/doc" "/bin" "/etc" ];
+    extraOutputsToInstall = [ "man" "doc" ];
+  };
+}
