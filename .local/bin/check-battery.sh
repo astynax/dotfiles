7718e6d208c52bdce7ef48deacbb64436e61ae0d
Aleksei Pirogov <astynax@users.noreply.github.com>
2020-12-08 10:32:48 +0300

    [cron] backup the crontab file, add a battery level checking script

---
+++ .local/bin/check-battery.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+DANGER_LEVEL=15
+
+PERCENT=$(\
+    upower -i /org/freedesktop/UPower/devices/battery_BAT0 \
+    | grep -Po '(?<=percentage:)\s+(\d+)%' \
+    | sed -e 's/[ %]//g' \
+    )
+
+if [ "$PERCENT" -lt "$DANGER_LEVEL" ]; then
+    zenity --warning --text "Charge me!" 2> /dev/null
+fi
