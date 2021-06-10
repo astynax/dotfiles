d33d8e15d10b8cf2fdaa593266660620e69b5a2f
Aleksei Pirogov <astynax@users.noreply.github.com>
2021-06-10 16:49:21 +0300

    [lein] update default profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,10 +1,10 @@
-{:user {:plugins [[lein-pprint "1.2.0"]
-                  [lein-kibit "0.1.6"]
+{:user {:plugins [[lein-pprint "1.3.2"]
+                  [lein-kibit "0.1.8"]
                   [lein-try "0.4.3"]
-                  [jonase/eastwood "0.2.7"]
-                  [lein-ancient "0.6.15"]
+                  [jonase/eastwood "0.4.3"]
+                  [lein-ancient "0.7.0"]
                   ]
         :dependencies [[slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}
- :repl {:dependencies [^:displace [org.clojure/clojure "1.9.0"]]}
+ :repl {:dependencies [^:displace [org.clojure/clojure "1.9.3"]]}
  }
