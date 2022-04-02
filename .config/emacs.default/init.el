0810ef4637ec516b9b06cd2f4fc8d1cbd53effce
Aleksei Pirogov <astynax@users.noreply.github.com>
2022-04-02 18:01:24 +0300

    [emacs] tweak the org link insertion

--- .config/emacs.default/init.el
+++ .config/emacs.default/init.el
@@ -2149,16 +2149,45 @@ _j_ ^ ^ _l_ _=_:equalize
       (if path
           (if (or (my/github/commit-hash-p ref) (= 4 arg))
               (insert
-               (format "[[%s][%s%s]]"
-                       url
-                       path
-                       (if line (format ":%s" line) "")))
+               (org-make-link-string
+                url
+                (format "%s%s" path (if line (format ":%s" line) ""))))
             (message "%s" "Non-local ref!"))
         (message "%s" "Non-github link!"))))
+
+  ;; src: https://xenodium.com/emacs-dwim-do-what-i-mean/
+  (defun my/org-insert-link-dwim ()
+    "Like `org-insert-link' but with personal dwim preferences."
+    (interactive)
+    (let* ((point-in-link (org-in-regexp org-link-any-re 1))
+           (clipboard-url (when (string-match-p "^http" (current-kill 0))
+                            (current-kill 0)))
+           (region-content (when (region-active-p)
+                             (buffer-substring-no-properties (region-beginning)
+                                                             (region-end)))))
+      (cond ((and region-content clipboard-url (not point-in-link))
+             (delete-region (region-beginning) (region-end))
+             (insert (org-make-link-string clipboard-url region-content)))
+            ((and clipboard-url (not point-in-link))
+             (insert (org-make-link-string
+                      clipboard-url
+                      (read-string
+                       "title: "
+                       (with-current-buffer
+                           (url-retrieve-synchronously clipboard-url)
+                         (dom-text
+                          (car
+                           (dom-by-tag (libxml-parse-html-region
+                                        (point-min)
+                                        (point-max))
+                                       'title))))))))
+            (t
+             (call-interactively 'org-insert-link)))))
 
   :bind
   (:map
    org-mode-map
+   ([remap org-insert-link] . my/org-insert-link-dwim)
    ("C-c l" . my/org/capture-gh-link)))
 
 (def-package my/org-slideshow
