30ae22c8b7d047d4eba42cb1139a92565dc66b21
Aleksei Pirogov <astynax@users.noreply.github.com>
2022-04-02 18:00:33 +0300

    [vortaro] make possible to jump to the next dictionary from the menu

--- .local/bin/vortaro.py
+++ .local/bin/vortaro.py
@@ -3,7 +3,7 @@
 from plumbum import local
 
 
-ROFI = local['rofi']['-dmenu', '-disable-history', '-p', 'Vortaro']
+ROFI = local['rofi']['-dmenu', '-disable-history']
 INPUT = local['zenity']['--entry', '--title=Vortaro']
 CB = local['xsel']['-op']
 NOTIFY = local['notify-send']
@@ -12,6 +12,7 @@ DICT = local['dict']
 DICTS = (
     'fd-eng-rus',
     'wn',
+    'gcide',
 )
 
 
@@ -24,21 +25,31 @@ def query(word, dicts=DICTS):
         return
     code, out, err = DICT['-d', dicts[0], word].run(retcode=None)
     if code == 0:
+        # single match
         NOTIFY(out)
         return
     if code == 20:
+        # no matches in a current dictionary, go to the next one
         query(word, dicts[1:])
         return
     if code == 21:
+        # no matches, but there are some similar words
         _, l, *_ = err.split('\n')
         _, *ws = l.split()
-        _, new_word, _ = (ROFI << '\n'.join(ws)).run(retcode=None)
+        exact_word = word + '!'
+        items = '\n'.join([exact_word] + ws)
+        prompt = 'Vortaro ({})'.format(','.join(dicts))
+        _, new_word, _ = (ROFI['-p', prompt] << items).run(retcode=None)
         new_word = new_word.strip()
-        if new_word:
+        if new_word == exact_word:
+            # the same word was selected, skip current dictionary
+            query(word, dicts[1:])
+        elif new_word:
+            # another candidate was chosen
+            # get it from the current dictionary
             query(new_word, dicts)
         return
     NOTIFY('Unexpected exitcode: {}\n\n{}'.format(code, err))
-
 
 def main():
     _, word, _ = CB.run()
