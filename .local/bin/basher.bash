ee0b7ca4f5ae817a35e96ce61dec14e6b12f997a
Aleksei Pirogov <astynax@users.noreply.github.com>
2022-10-23 17:02:47 +0400

    [bin] make basher.bash CWD-preserving

--- .local/bin/basher.bash
+++ .local/bin/basher.bash
@@ -7,11 +7,19 @@ if [[ -z "$1" ]]; then
         echo "Usage: basher.bash cmd line to run"
         exit
     fi
-    CMD=$(cat $F)
-    (setsid $CMD &)
+    mapfile -t LINES < $F
+    DIR=${LINES[0]}
+    if [[ -d "$DIR" ]]; then
+        cd "$DIR"
+        CMD=${LINES[1]}
+        (setsid $CMD &)
+    else
+        echo "Wrong CWD: $DIR"
+    fi
     exit
 else
-    echo $* > $F
+    pwd > $F
+    echo $* >> $F
     ID=$(uuidgen)
     systemctl --user start basher@"$ID".service
 fi
